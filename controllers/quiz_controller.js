aeefa5a515661e74f6850abb88ac21e10f7ef306
juandafg <juandafg@hotmail.com>
2015-08-04 00:56:18 +0200

    Final tema 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,12 +19,21 @@ exports.load = function(req, res, next, quizId) {
 };
 
 // GET /quizes
-exports.index = function(req, res) {
-    models.Quiz.findAll().then(
-		function(quizes) {
-				res.render('quizes/index.ejs', {quizes: quizes});
-	})
+
+exports.index = function(req, res) {  
+/*
+  var options = {};
+  if(req.user){
+    options.where = {UserId: req.user.id}
+  }
+*/  
+  models.Quiz.findAll(options).then(
+    function(quizes) {
+      res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+    }
+  ).catch(function(error){next(error)});
 };
+
 
 // GET /quizes/:id
 exports.show = function(req, res) {
